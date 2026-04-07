e9bf5b5cabc92e1c380ceb4875bd7662e867344a
Zachary Welz <welz.zach@gmail.com>
2026-04-07 10:09:03 -0400

    Cleanup for 0.3.0 release

--- src/holonic/__init__.py
+++ src/holonic/__init__.py
@@ -4,7 +4,7 @@ A lightweight Python client for building holonic knowledge graphs
 backed by rdflib, Apache Jena Fuseki, or any SPARQL-compliant store.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 from holonic.backends import GraphBackend, RdflibBackend
 from holonic.client import HolonicDataset
